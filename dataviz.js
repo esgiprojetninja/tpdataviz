4cd138ec42da021fd578537e76e10d5053212a09
Roland Kullet <renaud.bellec.3@gmail.com>
2017-06-06 17:48:09 +0200

    started

---
+++ dataviz.js
@@ -0,0 +1,38 @@
+var relations;
+$(document).ready(function(){
+	// Pas de cache sur les requête IMPORTANT !
+	$.ajaxSetup({ cache: false });
+
+	/***
+		On définit ici les fonctions de base qui vont nous servir à la récupération des données
+		Je ne définis que le GET ici, mais il est possible d'utiliser POST pour récupérer ses données (on le verra dans un prochain TP)
+	****/
+	function getRequest(url, callback) {
+		$.get(url, function(data) {
+			data = $.parseJSON(data);
+			callback(data);
+		});
+	}
+
+	/***************************************
+		QUESTION 1 : PIE CHART : Visite par marque
+	****************************************/
+	getRequest("liste_amis_user.php?user=5", function(data) {
+        relations = data;
+        cleanedData = data.map((item, index) => [
+            item[2],
+            index + 1
+        ]);
+        var plot1 = $.jqplot("chart1", [cleanedData], {
+            title: "Nombre d'amis au fil du mois",
+            axes: {
+                xaxis: {
+                    renderer: $.jqplot.DateAxisRenderer
+                }
+            },
+            series:[{lineWidth:4, markerOptions:{style:'square'}}]
+        })
+		console.log(data);
+        console.log(cleanedData);
+	});
+});
---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Data Vizualisation - TP1</title>
+		<!-- Inclusion CSS (librairie + perso) -->
+		<link rel="stylesheet" type="text/css" href="jquery.jqplot.min.css">
+		<link rel="stylesheet" type="text/css" href="dataviz.css">
+
+		<!-- Inclusion JS (librairie + scripts de création de graph) -->
+		<script type="text/javascript" src="jquery.js"></script>
+		<script type="text/javascript" src="jquery.jqplot.min.js"></script>
+
+		<script type="text/javascript" src="dataviz.js"></script>
+	</head>
+	<body>
+		<?php include ('header.php'); ?>
+		<div id="content">
+      <div id="chart1"></div>
+		</div>
+		<?php include ('footer.php'); ?>
+	</body>
+</html>
